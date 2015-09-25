32ffcdb5c22ba82864b2ab14c6fd9528e41dbabe
Ross Heflin <ross.heflin@vokal.io>
2015-09-25 09:03:10 -0500

    Works.  For a certain definition of works.

---
+++ app.py
@@ -0,0 +1,105 @@
+import signal # signal-related #@$#
+import sys
+
+import json
+
+from bottle import run, Bottle, request, response
+app = Bottle()
+
+from db import sql, sql_value
+
+## Signal Handler
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+NUM_DAYS = 1
+
+ALL_PLAYERS_STATS = """
+SELECT *
+FROM Player;
+"""
+
+DAILY_KILL_COUNT = """
+	SELECT COUNT(*) AS value
+	FROM   killlog
+	WHERE  FROM_UNIXTIME(killtime) >= NOW() - INTERVAL 2 DAY
+	AND    attacker = %s
+"""
+
+DAILY_DEATH_COUNT = """
+	SELECT COUNT(*) AS value
+	FROM   killlog
+	WHERE  FROM_UNIXTIME(killtime) >= NOW() - INTERVAL 2 DAY
+	AND    victim = %s
+"""
+
+DAILY_DOMINATIONS_COUNT = """
+	SELECT SUM(dominated) AS value
+	FROM   killlog
+	WHERE  FROM_UNIXTIME(killtime) >= NOW() - INTERVAL 2 DAY
+	AND    attacker = %s
+"""
+
+DAILY_REVENGES_COUNT = """
+	SELECT SUM(revenge) AS value
+	FROM   killlog
+	WHERE  FROM_UNIXTIME(killtime) >= NOW() - INTERVAL 2 DAY
+	AND    attacker = %s
+"""
+
+
+ROUTES = {
+  'daily-kills':        r'/player/<steamId>/daily-kills',
+  'all-players-stats':  r'/players',
+  'player-stat':        r'/player/<steamId>',
+  'root':               r'/',
+}
+
+
+@app.hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
+@app.route(ROUTES['root'])
+def root():
+    return {"routes": ROUTES}
+
+
+@app.route(ROUTES['all-players-stats'])
+def all_player_stats():
+    players = sql(ALL_PLAYERS_STATS)
+    return {"results": players}
+
+
+@app.route(ROUTES['player-stat'])
+def player_stats(steamId):
+    print("steamId:", steamId)
+    player = sql("SELECT * FROM Player WHERE steamId = %s", steamId)[0]
+    return {"result": player}
+
+
+@app.route(ROUTES['daily-kills'])
+def daily_kills(steamId):
+    result = {}
+
+    # TODO: make this suck less
+    result['kills'] = int(sql_value(DAILY_KILL_COUNT, steamId))
+    result['deaths'] = int(sql_value(DAILY_DEATH_COUNT, steamId))
+    result['dominations'] = int(sql_value(DAILY_DOMINATIONS_COUNT, steamId))
+    result['revenges'] = int(sql_value(DAILY_REVENGES_COUNT, steamId))
+    print(result)
+
+    return {"result": result}
+
+
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=3000, debug=True)
---
+++ db.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import atexit
+import json
+import os
+from urllib.parse import urlparse
+
+import pymysql
+import pymysql.cursors
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+## arg handling
+parsed = urlparse(DATABASE_URL)
+
+host = parsed.hostname
+port = parsed.port
+username = parsed.username
+password = parsed.password
+port = parsed.port
+db =  parsed.path[1:] # ignore / on /tf2stats
+
+
+## Connect to the database
+connection = pymysql.connect(host=host,
+                             user=username,
+                             password=password,
+                             db=db,
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+## close connection on exit
+atexit.register(lambda: connection.close())
+
+
+## so I don't have to care about cursors
+def sql(*args):
+    with connection.cursor() as cursor:
+        cursor.execute(*args)
+        return cursor.fetchall()
+
+def sql_value(*args):
+    with connection.cursor() as cursor:
+        cursor.execute(*args)
+        return cursor.fetchone().get('value')
